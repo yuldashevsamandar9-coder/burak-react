afef98013462a7a421ec9ab180a5f22a63031792
Sam <yuldashevsamandar9@gmail.com>
2026-07-01 00:30:01 +0900

    fix: home navbar refactoring detailing

--- src/app/components/headers/HomeNavbar.tsx
+++ src/app/components/headers/HomeNavbar.tsx
@@ -2,54 +2,69 @@ import { Box, Button, Container, Stack } from "@mui/material";
 import { NavLink } from "react-router-dom";
 
 export function HomeNavbar() {
-    const authMember = null;
-    return (<div className="home-navbar">
-        <Container sx={{ mt: "55px", height: "642" }}>
-            <Stack sx={{ height: "50px" }} flexDirection={"row"} justifyContent={"space-between"} alignItems={"center"}>
-                <Box>
-                    <NavLink to={"/"}>
-                        <img style={{ width: "125px", height: "30px" }} src="/icons/burak.svg" />
-                    </NavLink>
+  const authMember = null;
+  return (
+    <div className="home-navbar">
+      <Container className="navbar-container">
+        <Stack className="menu">
+          <Box>
+            <NavLink to={"/"}>
+              <img className="brand-logo" src="/icons/burak.svg" />
+            </NavLink>
+          </Box>
+          <Stack className="links">
+            <Box className={"hover-line"}>
+              <NavLink to={"/"} activeClassName={"underline"}>
+                Home
+              </NavLink>
+            </Box>
+            <Box className={"hover-line"}>
+              <NavLink to={"/products"} activeClassName={"underline"}>
+                Products
+              </NavLink>
+            </Box>
+            <Box className={"hover-line"}>
+              {authMember ? (
+                <Box className={"hover-line"}>
+                  <NavLink to={"/orders"} activeClassName={"underline"}>
+                    Orders
+                  </NavLink>
                 </Box>
-                <Stack flexDirection={"row"} justifyContent={"space-between"} minWidth={"700px"} alignItems={"center"}>
-                    <Box className={"hover-line"}>
-                        <NavLink to={"/"} activeClassName={"underline"}>
-                            Home
-                        </NavLink>
-                    </Box>
-                    <Box className={"hover-line"}>
-                        <NavLink to={"/products"} activeClassName={"underline"}>
-                            Products
-                        </NavLink>
-                    </Box>
-                    <Box className={"hover-line"}>
-                        {authMember ? (<Box className={"hover-line"}>
-                            <NavLink to={"/orders"} activeClassName={"underline"}>
-                                Orders
-                            </NavLink>
-                        </Box>
-                        ) : null}
-                    </Box>
-                    <Box className={"hover-line"}>
-                        {authMember ? (<Box className={"hover-line"}>
-                            <NavLink to={"/member-page"} activeClassName={"underline"}>
-                                My Page
-                            </NavLink>
-                        </Box>
-                        ) : null}
-                    </Box>
-                    <Box className={"hover-line"}>
-                        <NavLink to={"/help"} activeClassName={"underline"}>
-                            Help
-                        </NavLink>
-                    </Box>
-                    {/* Basket */}
+              ) : null}
+            </Box>
+            <Box className={"hover-line"}>
+              {authMember ? (
+                <Box className={"hover-line"}>
+                  <NavLink to={"/member-page"} activeClassName={"underline"}>
+                    My Page
+                  </NavLink>
+                </Box>
+              ) : null}
+            </Box>
+            <Box className={"hover-line"}>
+              <NavLink to={"/help"} activeClassName={"underline"}>
+                Help
+              </NavLink>
+            </Box>
+            {/* Basket */}
 
-                    {!authMember ? (<Box><Button variant="contained" style={{ background: "#3776CC", color: "#f8f8ff" }}>LOGIN</Button></Box>) : (<img />)}
-                </Stack>
-            </Stack>
-            <Stack>Detail</Stack>
-        </Container>
+            {!authMember ? (
+              <Box>
+                <Button variant="contained" className="login-button">
+                  LOGIN
+                </Button>
+              </Box>
+            ) : (
+              <img
+                className="user-avatar"
+                src={"/icons/default-user.svg"}
+                aria-haspopup={"true"}
+              />
+            )}
+          </Stack>
+        </Stack>
+        <Stack>Detail</Stack>
+      </Container>
     </div>
-    );
+  );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -4,14 +4,13 @@ import { Provider } from "react-redux";
 import { store } from "./app/store";
 import App from "./app/App.tsx";
 import { BrowserRouter as Router } from "react-router-dom";
+
 createRoot(document.getElementById("root")!).render(
-    <React.StrictMode>
-        <Provider store={store}>
-            <Router>
-                <App />
-            </Router>
-        </Provider>
-    </React.StrictMode>,
+  <React.StrictMode>
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
+  </React.StrictMode>,
 );
-
-
