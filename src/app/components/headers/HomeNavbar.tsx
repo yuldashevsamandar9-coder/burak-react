dc0df30ea291c74392e1177ffeefb6f9f8d5ef89
Sam <yuldashevsamandar9@gmail.com>
2026-07-01 12:08:24 +0900

    fix: modify homeNavbar header component

--- src/app/components/headers/HomeNavbar.tsx
+++ src/app/components/headers/HomeNavbar.tsx
@@ -63,7 +63,26 @@ export function HomeNavbar() {
             )}
           </Stack>
         </Stack>
-        <Stack>Detail</Stack>
+        <Stack className={"header-frame"}>
+          <Stack className={"detail"}>
+            <Box className={"head-main-txt"}>
+              Worlds Most Delicious Cousine{" "}
+            </Box>
+            <Box className={"wel-txt"}> The Choice, not just a choice</Box>
+            <Box className={"service-txt"}>24 hours service</Box>
+            <Box className={"signup"}>
+              {" "}
+              {!authMember ? (
+                <Button variant={"contained"} className={"signup-button"}>
+                  SIGNUP
+                </Button>
+              ) : null}
+            </Box>
+          </Stack>
+          <Stack className={"logo-frame "}>
+            <div className={"logo-img"}></div>
+          </Stack>
+        </Stack>
       </Container>
     </div>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
 import App from "./app/App.tsx";
+import "../css/app.css";
+import "../css/navbar.css";
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
