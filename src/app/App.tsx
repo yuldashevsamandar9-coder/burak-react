d262fc43dc0273401222c2017a3db516e5eb13ed
Sam <yuldashevsamandar9@gmail.com>
2026-07-17 00:26:20 +0900

    fix: modify screen componentment

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -35,8 +35,8 @@ function App() {
           <HelpPage />
         </Route>
         <Route path="/">
-          <Test />
-          {/* <HomePage /> */}
+          {/* <Test /> */}
+          <HomePage />
         </Route>
       </Switch>
       <Footer />
