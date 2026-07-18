afd1bf340255fd156a001e6f1a7c211a51d412da
Sam <yuldashevsamandar9@gmail.com>
2026-07-19 08:31:56 +0900

    feat: middleware intagration redux-logger

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,13 +1,14 @@
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
 import HomePageReducer from "./screens/homePage/slice";
+import reduxLogger from "redux-logger";
+
 //import ProductsPageReducer from "./screens/productsPage/slice";
 //import OrdersPageReducer from "./screens/ordersPage/slice";
-//import reduxLogger from "redux-logger";
 
 export const store = configureStore({
-  // middleware: (getDefaultMiddleware) =>
-  //   // @ts-ignore
-  //   getDefaultMiddleware().concat(reduxLogger),
+  middleware: (getDefaultMiddleware) =>
+    //  @ts-ignore
+    getDefaultMiddleware().concat(reduxLogger),
   reducer: {
     homePage: HomePageReducer,
     //productsPage: ProductsPageReducer,
