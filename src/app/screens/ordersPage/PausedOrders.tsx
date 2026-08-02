f3850deade0908ee8a7b0efeb2e5c829f3893383
Sam <yuldashevsamandar9@gmail.com>
2026-08-02 10:50:42 +0900

    fix: processOrders and finishedOrders modify

--- src/app/screens/ordersPage/PausedOrders.tsx
+++ src/app/screens/ordersPage/PausedOrders.tsx
@@ -20,11 +20,11 @@ const pausedOrdersRetriever = createSelector(
 
 export default function PausedOrders() {
   const { pausedOrders } = useSelector(pausedOrdersRetriever);
-  console.log("pausedOrders:", pausedOrders);
+
   return (
     <TabPanel value={"1"}>
       <Stack>
-        {pausedOrders.map((order: Order) => {
+        {pausedOrders?.map((order: Order) => {
           return (
             <Box key={order._id} className={"order-main-box"}>
               <Box className={"order-box-scroll"}>
--- src/app/services/OrderService.ts
+++ src/app/services/OrderService.ts
@@ -34,7 +34,10 @@ class OrderService {
       const url = `${this.path}/order/all`;
       const query = `?page=${input.page}&limit=${input.limit}&orderStatus=${input.orderStatus}`;
       const result = await axios.get(url + query, { withCredentials: true });
-      console.log("getMyOrders:", result);
+      //   console.log("getMyOrders:", result);
+
+      console.log("STATUS:", input.orderStatus);
+      console.log("RESULT.DATA:", result.data);
 
       return result.data;
     } catch (err) {
